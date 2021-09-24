8b1edb5fcd20c6c6c7fe02546a5eaef2ebbcde8f
mmateogr <mateo.gomezrios@hotmail.com>
2021-09-24 17:08:54 -0300

    Primera carga de mi proyecto

---
+++ javasc/app.js
@@ -0,0 +1,34 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+    menu = document.querySelector('#menu')
+
+iconoMenu.addEventListener('click', (e) => {
+
+
+    menu.classList.toggle('active');
+    document.body.classList.toggle('.opacity');
+
+    const rutaActual = e.target.getAttribute('src');
+
+    if(rutaActual == 'imagenes/open-menu.png'|| rutaActual == '../imagenes/open-menu.png'){
+        e.target.setAttribute('src','imagenes/open-menu2.png');
+    }else{
+        e.target.setAttribute('src','imagenes/open-menu.png');
+    }    
+})
+
+const bloque    = document.querySelectorAll('.bloque')
+const h3        = document.querySelectorAll('.h3')
+
+h3.forEach(  (  cadaH3 , i )=>{
+    h3[i].addEventListener('click', ()=>{
+        
+        bloque.forEach( ( cadaBloque , i )=>{
+            bloque[i].classList.remove('activo')
+
+        })
+        bloque[i].classList.add('activo')
+
+    })
+
+})
+
