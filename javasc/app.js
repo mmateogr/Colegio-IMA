99f5a8dc807e50d35b3326a49a1d38530956875e
mmateogr <mateo.gomezrios@hotmail.com>
2021-09-27 17:31:32 -0300

    Corrección de error de iconos en JS

--- javasc/app.js
+++ javasc/app.js
@@ -10,9 +10,9 @@ iconoMenu.addEventListener('click', (e) => {
     const rutaActual = e.target.getAttribute('src');
 
     if(rutaActual == 'imagenes/open-menu.png'|| rutaActual == '../imagenes/open-menu.png'){
-        e.target.setAttribute('src','imagenes/open-menu2.png');
+        e.target.setAttribute('src','imagenes/open-menu2.png')|| e.target.setAttribute('src','../imagenes/open-menu2.png');
     }else{
-        e.target.setAttribute('src','imagenes/open-menu.png');
+        e.target.setAttribute('src','imagenes/open-menu.png')|| e.target.setAttribute('src','../imagenes/open-menu.png');
     }    
 })
 
