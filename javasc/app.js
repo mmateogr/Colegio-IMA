be3f3f0b56e88dcb60d66617cb0cf519921229da
mmateogr <mateo.gomezrios@hotmail.com>
2021-09-28 14:34:15 -0300

    Corrección del botón ménú hamburguesa index

--- javasc/app.js
+++ javasc/app.js
@@ -9,11 +9,18 @@ iconoMenu.addEventListener('click', (e) => {
 
     const rutaActual = e.target.getAttribute('src');
 
-    if(rutaActual == 'imagenes/open-menu.png'|| rutaActual == '../imagenes/open-menu.png'){
-        e.target.setAttribute('src','imagenes/open-menu2.png')|| e.target.setAttribute('src','../imagenes/open-menu2.png');
-    }else{
-        e.target.setAttribute('src','imagenes/open-menu.png')|| e.target.setAttribute('src','../imagenes/open-menu.png');
-    }    
+    switch (rutaActual) {
+        case 'imagenes/open-menu.png':
+            e.target.setAttribute('src','imagenes/open-menu2.png');
+            break;
+        case '../imagenes/open-menu.png':
+            e.target.setAttribute('src','../imagenes/open-menu2.png');
+            break;
+        default: if (rutaActual == 'imagenes/open-menu2.png') {
+            e.target.setAttribute('src','imagenes/open-menu.png');
+        }else e.target.setAttribute('src','../imagenes/open-menu.png'); 
+    }
+
 })
 
 const bloque    = document.querySelectorAll('.bloque')
