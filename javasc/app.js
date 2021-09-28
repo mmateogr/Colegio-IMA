a71e7da629fa663a00cfc07b79ee340cfcced1bd
mmateogr <mateo.gomezrios@hotmail.com>
2021-09-28 20:42:15 -0300

    Link en h1; corrección de párrafo en elColegio para celular; modificaciones en mismo párrafo de elColegio

--- javasc/app.js
+++ javasc/app.js
@@ -5,7 +5,6 @@ iconoMenu.addEventListener('click', (e) => {
 
 
     menu.classList.toggle('active');
-    document.body.classList.toggle('.opacity');
 
     const rutaActual = e.target.getAttribute('src');
 
