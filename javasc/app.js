e13de84b55d07e0d57788ee193459a7c6c5bb4f2
mmateogr <mateo.gomezrios@hotmail.com>
2021-11-02 12:41:03 -0300

    Agregado de formulario

--- javasc/app.js
+++ javasc/app.js
@@ -38,3 +38,23 @@ tituloAcordeon.forEach(  (  cadaH3 , i )=>{
 
 })
 
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        alert('Gracias por contactarnos, te contestaremos pronto!')
+    }
+}
